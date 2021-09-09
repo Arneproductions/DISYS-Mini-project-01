e63e5f13fc3762fb0877b0a9084b55f0bf970b47
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-09 21:44:59 +0200

    Add initialization to main

--- main.go
+++ main.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"time"
+
 	"dk.disys.phil/model"
 )
 
 func main() {
-	left := model.NewFork()
-	right := model.NewFork()
-	p := model.NewPhilosopher(right, left)
+	forks := initForks(5)
+	phils := initPhils(forks)
 
-	fmt.Println(p)
+	for {
+		for idx, val := range phils {
+			fmt.Printf("Philosopher %d - %s\n", (idx + 1), val.GetStatus())
+		}
+
+		fmt.Println("=============")
+
+		time.Sleep(400)
+	}
+}
+
+func initForks(amount int) []model.Fork {
+	forks := make([]model.Fork, 0)
+
+	for i := 0; i < amount; i++ {
+		forks = append(forks, model.NewFork())
+	}
+
+	return forks
+}
+
+func initPhils(forks []model.Fork) []model.Philosopher {
+	phils := make([]model.Philosopher, 0)
+
+	for i := 0; i < len(forks); i++ {
+		if i%len(forks) == 0 {
+			phils = append(phils, model.NewPhilosopher(forks[len(forks)-1], forks[i]))
+		} else {
+			phils = append(phils, model.NewPhilosopher(forks[len(forks)-1], forks[i]))
+		}
+	}
+
+	return phils
 }
